7bedcb2c032666e2a42bcc67649977268ce856db
kmanske343 <94566431+kmanske343@users.noreply.github.com>
2023-10-22 18:25:37 -0500

    Update dataCapture.py

--- dataCapture.py
+++ dataCapture.py
@@ -1,1 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
+url = 'https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt'
+html = requests.get(url)
+
+s = BeautifulSoup(html.content, 'html.parser')
+
+string = str(s)
+dataByYear = []
+
+
+for letter in string:
+    triplet = [] 
+    dataByYear.append(letter)
+
+print(dataByYear)
