5e88bb0be3ae9c8ad4a1ec2472bf17c90479f12a
Kieran Mansk <kmanske343@lsr7.net>
2023-10-23 09:57:54 -0500

    text

--- dataCapture.py
+++ dataCapture.py
@@ -3,32 +3,35 @@ from bs4 import BeautifulSoup
 
 url = 'https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt'
 html = requests.get(url)
+# print(html.text)
 
-s = BeautifulSoup(html.content, 'html.parser')
+# s = BeautifulSoup(html.content, 'html.parser')
 
-raw = str(s)
-dataByYear = []
-lines = []
+# raw = str(s)
+# dataByYear = []
+# lines = []
+
+# for letter in raw:
+#     dataByYear.append(letter)
+#     if letter == "\n":
+#         lines.append(dataByYear)
+#         dataByYear = []
+
+for line in html.text.split('\n'):
+#     line = str(line)
+    # print(line)
+    print(line.split('\s+'))
+    # for letter in line:
+    #     if letter == " ":
+    #         print(line.split('\s+'))
 
 
 
 
-for letter in raw:
-    dataByYear.append(letter)
-    if letter == "\n":
-        lines.append(dataByYear)
-        dataByYear = []
+# for line in lines:
+#     lines[lines.index(line)] = ''.join(line)
 
-for line in lines:
-    for letter in line:
-        if letter == " ":
-            line.remove(letter)
+# lines = lines[5:]
 
-for line in lines:
-    lines[lines.index(line)] = ''.join(line)
-
-#remove line 1-4 from lines
-lines = lines[5:]
-
-for i in range(len(lines)):
-    print(lines[i])
+# for i in range(len(lines)):
+#     print(lines[i])
