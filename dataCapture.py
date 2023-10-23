4268cfc759883e5e91e86243664730764b2f47a4
Kieran Mansk <kmanske343@lsr7.net>
2023-10-23 09:38:49 -0500

    most data is minipulated

--- dataCapture.py
+++ dataCapture.py
@@ -6,19 +6,29 @@ html = requests.get(url)
 
 s = BeautifulSoup(html.content, 'html.parser')
 
-string = str(s)
+raw = str(s)
 dataByYear = []
 lines = []
-string.strip()
 
 
 
-for letter in string:
-    triplet = ""
+
+for letter in raw:
     dataByYear.append(letter)
+    if letter == "\n":
+        lines.append(dataByYear)
+        dataByYear = []
 
-for character in dataByYear:
-    if character.isnumeric() == False:
-        dataByYear.pop(character)
+for line in lines:
+    for letter in line:
+        if letter == " ":
+            line.remove(letter)
 
-print(dataByYear)
+for line in lines:
+    lines[lines.index(line)] = ''.join(line)
+
+#remove line 1-4 from lines
+lines = lines[5:]
+
+for i in range(len(lines)):
+    print(lines[i])
