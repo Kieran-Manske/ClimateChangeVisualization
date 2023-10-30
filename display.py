2fa48bc91405164b660d043266dd6f24bf0f7333
Kieran Mansk <kmanske343@lsr7.net>
2023-10-30 09:44:44 -0500

    window open

--- display.py
+++ display.py
@@ -1,9 +1,32 @@
-import dataCapture
+from dataCapture import finalData
 from kivy.app import App 
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 from kivy.lang import Builder
 from kivy.uix.image import Image
 from kivy.core.window import Window
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
+
+class ClimateApp(App):
+    def build(self):
+        game = gridLayout()
+        return game
+    
+def colorSpectrum(finalData):
+    
+
+class gridLayout(GridLayout):
+    def __init__(self, **args):
+        super(gridLayout, self).__init__(**args)
+        self.cols = 2
+        self.rows = 1
+        world = Image(source='World_green_cnamed.jpg')
+        self.add_widget(world)
+        
+        self.add_widget(Button(text = "Hello World"))
+
+root = ClimateApp()
+root.run()
 
